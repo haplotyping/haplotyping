dc89ccb3d63346196a6517fce421cc5ba37c1dbe
Matthijs Brouwer <matthijs@brouwer.info>
2023-09-22 13:08:23 +0200

    update

--- haplotyping/service/split.py
+++ haplotyping/service/split.py
@@ -230,7 +230,7 @@ class Split:
                     for kmerId in read:
                         if not kmerId in kmerDict:
                             entry = ckmerTable[kmerId]
-                            kmerDict[kmerId] = (entry[0].decode("ascii"),entry[1].decode("ascii"),entry[2])
+                            kmerDict[kmerId] = (entry[0].decode("ascii"),entry[1].decode("ascii"),int(entry[2]))
                         kmerList.append(kmerDict[kmerId])
                     response.append({"kmers": kmerList, "number": int(item[1])})
                 n+=item[0]
@@ -377,7 +377,7 @@ class Split:
         readInfoTable = h5file.get("/relations/readInfo")
         (ckmerRow,id,cache) = Split._findItem(ckmer,ckmerTable)
         if ckmerRow:
-            kmerDict = {id: (ckmerRow[0].decode("ascii"),ckmerRow[1].decode("ascii"),ckmerRow[2])}
+            kmerDict = {id: (ckmerRow[0].decode("ascii"),ckmerRow[1].decode("ascii"),int(ckmerRow[2]))}
             partitionRow = readPartitionTable[ckmerRow[5]]
             readDataList = readDataTable[partitionRow[0][0]:partitionRow[0][0]+partitionRow[0][1]]
             readInfoList = readInfoTable[partitionRow[1][0]:partitionRow[1][0]+partitionRow[1][1]]
@@ -407,7 +407,7 @@ class Split:
             ckmer = ckmerList[i]
             (ckmerRow,id,cache) = Split._findItem(ckmerList[i],ckmerTable,start,number,cache)
             if ckmerRow:
-                kmerDict[id] = (ckmerRow[0].decode("ascii"),ckmerRow[1].decode("ascii"),ckmerRow[2])
+                kmerDict[id] = (ckmerRow[0].decode("ascii"),ckmerRow[1].decode("ascii"),int(ckmerRow[2]))
                 kmerIds.append(id)
                 partitions.add(ckmerRow[5])
                 start = id+1
